74d1fbd430b6cc45dd7c4fe7576f0019bd943f8e
hkh9601 <hkh9601@gmail.com>
2022-06-14 05:55:35 +0900

    크롤링배치

---
+++ model/Building.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+
+const ProcessSubSchema = mongoose.Schema({
+  dayProcess: String,
+  progress: String,
+  date: String,
+});
+
+const TenantsSubSchema = mongoose.Schema({
+  tenantName: String,
+  location: String,
+  date: String,
+});
+
+const pointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+});
+
+const BuildingSchema = mongoose.Schema(
+  {
+    picture: {
+      type: [String],
+    },
+    auctionNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    buildingType: {
+      type: String,
+      enum: ['아파트', '주택', '다세대/다가구', '오피스텔/원룸'],
+      default: '아파트',
+      required: true,
+    },
+    squareMeters: {
+      type: String,
+      required: true,
+    },
+    connoisseur: {
+      type: String,
+      required: true,
+    },
+    lowestPrice: {
+      type: String,
+    },
+
+    deposit: String,
+
+    process: [ProcessSubSchema],
+
+    tenants: [TenantsSubSchema],
+
+    caution: String,
+
+    appraisal: {
+      type: String,
+      default: null,
+    },
+    coords: {
+      type: pointSchema,
+      index: '2dsphere',
+    },
+    user: {
+      type: [mongoose.Schema.Types.ObjectId],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Building', BuildingSchema);
